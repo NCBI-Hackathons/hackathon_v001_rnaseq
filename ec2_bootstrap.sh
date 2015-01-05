656dc0f5b305084e6e9239d182c168ca5fa30d3e
Abhinav Nellore <anellore@gmail.com>
2015-01-05 16:00:54 -0500

    corrects directory in bootstrap script

--- ec2_bootstrap.sh
+++ ec2_bootstrap.sh
@@ -9,8 +9,8 @@ cd $WORK
 # Download HISAT
 curl -O http://www.ccb.jhu.edu/software/hisat/downloads/hisat-0.1.2-beta-Linux_x86_64.zip || { echo "error downloading HISAT"; exit 1; }
 unzip hisat-0.1.2-beta-Linux_x86_64.zip || { echo "error unzipping HISAT"; exit 1; }
-sudo ln -s `pwd`/hisat-0.1.2-beta-Linux_x86_64/hisat /usr/local/bin || { echo "hisat is already installed"; }
-sudo ln -s `pwd`/hisat-0.1.2-beta-Linux_x86_64/hisat-build /usr/local/bin || { echo "hisat-build is already installed"; }
+sudo ln -s `pwd`/hisat-0.1.2-beta/hisat /usr/local/bin || { echo "hisat is already installed"; }
+sudo ln -s `pwd`/hisat-0.1.2-beta/hisat-build /usr/local/bin || { echo "hisat-build is already installed"; }
 # Download and make SAMTools
 curl -OL http://downloads.sourceforge.net/project/samtools/samtools/1.1/samtools-1.1.tar.bz2 || { echo 'curl failed' ; exit 1; }
 tar xvjf samtools-1.1.tar.bz2 || { echo "samtools untar+gunzip failed" ; exit 1; }
