e9cf9a9b3fec9de352d07afdab040e4a93d2cd89
Abhinav Nellore <anellore@gmail.com>
2015-01-05 15:50:25 -0500

    debugs bootstrap script

--- ec2_bootstrap.sh
+++ ec2_bootstrap.sh
@@ -11,12 +11,10 @@ unzip hisat-0.1.2-beta-Linux_x86_64.zip || { echo "error unzipping HISAT"; exit 
 sudo ln -s `pwd`/hisat-0.1.2-beta-Linux_x86_64/hisat /usr/local/
 sudo ln -s `pwd`/hisat-0.1.2-beta-Linux_x86_64/hisat-build /usr/local/bin
 # Download and make SAMTools
-sudo apt-get install -f ncurses-dev ncurses || { echo "curses installation failed" ; exit 1; }
-curl -O http://downloads.sourceforge.net/project/samtools/samtools/1.1/samtools-1.1.tar.bz2 || { echo 'curl failed' ; exit 1; }
+sudo apt-get install -f libncurses5-devn || { echo "curses installation failed" ; exit 1; }
+curl -OL http://downloads.sourceforge.net/project/samtools/samtools/1.1/samtools-1.1.tar.bz2 || { echo 'curl failed' ; exit 1; }
 tar xvjf samtools-1.1.tar.bz2 || { echo "samtools untar+gunzip failed" ; exit 1; }
 cd samtools-1.1
 sudo make || { echo 'samtools make failed'; exit 1; }
-cd ..
-sudo ln -s `pwd`/samtools-0.1.19/samtools /usr/local/bin
 # Download and install Bambino
 curl -OL https://cgwb.nci.nih.gov/cgi-bin/bambino?download_bambino_jar=bundle || { echo 'curl failed' ; exit 1; }
