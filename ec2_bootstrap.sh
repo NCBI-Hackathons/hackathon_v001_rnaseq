d664f0d9181a8b3fa12763bb784224505386c502
Abhinav Nellore <anellore@gmail.com>
2015-01-05 15:55:55 -0500

    does not exit script if ln -s fails

--- ec2_bootstrap.sh
+++ ec2_bootstrap.sh
@@ -8,8 +8,8 @@ cd $WORK
 # Download HISAT
 curl -O http://www.ccb.jhu.edu/software/hisat/downloads/hisat-0.1.2-beta-Linux_x86_64.zip || { echo "error downloading HISAT"; exit 1; }
 unzip hisat-0.1.2-beta-Linux_x86_64.zip || { echo "error unzipping HISAT"; exit 1; }
-sudo ln -s `pwd`/hisat-0.1.2-beta-Linux_x86_64/hisat /usr/local/  || exit 0;
-sudo ln -s `pwd`/hisat-0.1.2-beta-Linux_x86_64/hisat-build /usr/local/bin || exit 0;
+sudo ln -s `pwd`/hisat-0.1.2-beta-Linux_x86_64/hisat /usr/local/ || { echo "hisat is already installed"; }
+sudo ln -s `pwd`/hisat-0.1.2-beta-Linux_x86_64/hisat-build /usr/local/bin || { echo "hisat-build is already installed"; }
 # Download and make SAMTools
 sudo apt-get install -f libncurses5-devn || { echo "curses installation failed" ; exit 1; }
 curl -OL http://downloads.sourceforge.net/project/samtools/samtools/1.1/samtools-1.1.tar.bz2 || { echo 'curl failed' ; exit 1; }
