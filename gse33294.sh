dcce90f04972583c9fd3e0dee1667af214d5c468
lacuss <lacuss@gmail.com>
2015-01-06 15:15:23 -0500

    gse33294.sh

---
+++ gse33294.sh
@@ -0,0 +1,6 @@
+/blast/rna/sratoolkit.2.4.2-ubuntu64/bin/fastq-dump -I --split-files SRX103485
+/blast/rna/sratoolkit.2.4.2-ubuntu64/bin/fastq-dump -I --split-files SRX103486
+/blast/rna/sratoolkit.2.4.2-ubuntu64/bin/fastq-dump -I --split-files SRX103487
+/blast/rna/sratoolkit.2.4.2-ubuntu64/bin/fastq-dump -I --split-files SRX103488
+/blast/rna/sratoolkit.2.4.2-ubuntu64/bin/fastq-dump -I --split-files SRX103489
+/blast/rna/sratoolkit.2.4.2-ubuntu64/bin/fastq-dump -I --split-files SRX103490
